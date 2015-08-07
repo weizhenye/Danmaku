70265911ee1799cc7ad6941be3522a6035bf1a67
WeiZhenYe <weizhenye@gmail.com>
2015-08-08 01:54:39 +0800

    support canvas engine

--- parser/bilibili.js
+++ parser/bilibili.js
@@ -1,4 +1,4 @@
-function BilibiliParser(xmlDoc) {
+function BilibiliParser(xmlDoc, engine) {
   var elements = xmlDoc.getElementsByTagName('d');
   var comments = [];
   for (var i = elements.length - 1; i >= 0; i--) {
@@ -8,8 +8,8 @@ function BilibiliParser(xmlDoc) {
       var values = ele.getAttribute('p').split(',');
       var comment = {};
       var mode = values[1] * 1;
-      if (mode === 6) comment.mode = 'lefttoright';
-      else if (mode === 1) comment.mode = 'righttoleft';
+      if (mode === 6) comment.mode = 'ltr';
+      else if (mode === 1) comment.mode = 'rtl';
       else if (mode === 5) comment.mode = 'top';
       else if (mode === 4) comment.mode = 'bottom';
       else continue;
@@ -17,7 +17,7 @@ function BilibiliParser(xmlDoc) {
       comment.time = values[0] * 1;
       var color = parseInt(values[3]).toString(16);
       while (color.length < 6) color = '0' + color;
-      comment.style = {
+      var style = {
         color: '#' + color,
         fontSize: values[2] + 'px',
         textShadow: (color === '000000')
@@ -26,6 +26,17 @@ function BilibiliParser(xmlDoc) {
                       : '-1px -1px #000, -1px 1px #000, '
                         + '1px -1px #000, 1px 1px #000'
       };
+      var canvasStyle = {
+        font: values[2] + 'px sans-serif',
+        fillStyle: '#' + color,
+        strokeStyle: (color === '000000') ? '#fff' : '#000',
+      };
+      if (engine === 'DOM') comment.style = style;
+      else if (engine === 'canvas') comment.canvasStyle = canvasStyle;
+      else {
+        comment.style = style;
+        comment.canvasStyle = canvasStyle;
+      }
       comments.push(comment);
     }
   }
