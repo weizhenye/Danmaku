fa9cfdcb39cfcad7dbc3f4e4845c8e275bf45d17
David xu <xqdoo00o@gmail.com>
2022-05-02 15:38:05 +0800

    fix: canvas rendering blurry if dpr > 1 (#42)
    * fix: canvas rendering blurry if dpr > 1
    
    * fix: canvas speed
    
    * fix: format for CI

--- src/api/speed.js
+++ src/api/speed.js
@@ -11,7 +11,7 @@ export default {
     }
     this._.speed = s;
     if (this._.stage.width) {
-      this._.duration = this._.stage.width / s;
+      this._.duration = this._.stage.width / (this.engine === 'canvas' ? devicePixelRatio : 1) / s;
     }
     return s;
   }
