890a82349ecd25b5f12c66747764f8c85174af87
CY <shiyiya.11@gmail.com>
2023-04-04 22:54:35 +0800

    Update canvas.js

--- src/engine/canvas.js
+++ src/engine/canvas.js
@@ -1,4 +1,4 @@
-const dpr = typeof window !== undefined && window.devicePixelRatio || 1;
+const dpr = typeof window !== 'undefined' && window.devicePixelRatio || 1;
 
 export var canvasHeightCache = Object.create(null);
 
