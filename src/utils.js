28c66f63c8ab095152825240513176758b5ef76c
CY <shiyiya.11@gmail.com>
2023-04-04 13:56:47 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,9 @@
 export /* istanbul ignore next */ var raf =
-  window.requestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  function(cb) {
-    return setTimeout(cb, 50 / 3);
-  };
+  (typeof window != undefined &&
+    (window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame)) ||
+  function (cb) {
+    return setTimeout(cb, 50 / 3)
+  }
 
 export /* istanbul ignore next */ var caf =
   window.cancelAnimationFrame ||
