e91bd7ad2ef6c1f8d6bc40d71698594696996e1e
weizhenye <weizhenye@gmail.com>
2026-08-08 12:06:25 +0800

    2.0.10

--- dist/esm/danmaku.js
+++ dist/esm/danmaku.js
@@ -321,13 +321,16 @@ function now() {
 function allocate(cmt) {
   var that = this;
   var ct = this.media ? this.media.currentTime : now() / 1000;
-  var pbr = this.media ? this.media.playbackRate : 1;
   function willCollide(cr, cmt) {
     if (cmt.mode === 'top' || cmt.mode === 'bottom') {
       return ct - cr.time < that._.duration;
     }
+    // `ct`, `cr.time` and `that._.duration` all live on the same timeline
+    // (media time when bound to a media element, wall-clock time otherwise),
+    // so `ct - cr.time` is already the elapsed travelling time on it.
+    // Scaling it by `playbackRate` would count the rate twice.
     var crTotalWidth = that._.width + cr.width;
-    var crElapsed = crTotalWidth * (ct - cr.time) * pbr / that._.duration;
+    var crElapsed = crTotalWidth * (ct - cr.time) / that._.duration;
     if (cr.width > crElapsed) {
       return true;
     }
@@ -335,7 +338,7 @@ function allocate(cmt) {
     var crLeftTime = that._.duration + cr.time - ct;
     var cmtTotalWidth = that._.width + cmt.width;
     var cmtTime = that.media ? cmt.time : cmt._utc;
-    var cmtElapsed = cmtTotalWidth * (ct - cmtTime) * pbr / that._.duration;
+    var cmtElapsed = cmtTotalWidth * (ct - cmtTime) / that._.duration;
     var cmtArrival = that._.width - cmtElapsed;
     // (rtl mode) the left end of `cmt` reach the left side of stage
     var cmtArrivalTime = that._.duration * cmtArrival / (that._.width + cmt.width);
